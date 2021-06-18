f538b7e6fc81afc5d94bf3ea9f63ef0ea15fb95a
Jeroen Bobbeldijk <jeroen@klippa.com>
2021-06-18 12:11:03 +0200

    Update vue demo

--- demo-vue/nativescript.config.ts
+++ demo-vue/nativescript.config.ts
@@ -1,11 +1,11 @@
-import { NativeScriptConfig } from '@nativescript/core'
+import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
   id: 'org.nativescript.demovue',
-  appResourcesPath: 'app/App_Resources',
+  appPath: 'app',
+  appResourcesPath: 'App_Resources',
   android: {
     v8Flags: '--expose_gc',
-    markingMode: 'none',
-  },
-  appPath: 'app',
-} as NativeScriptConfig
+    markingMode: 'none'
+  }
+} as NativeScriptConfig;
